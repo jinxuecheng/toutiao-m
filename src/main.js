c83cb6bcb41a17786bae24822b51863e4459109d
jinxuecheng <jin19881205@gmail.com>
2022-08-22 22:47:59 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 加载核心组件库
+import Vant from 'vant'
+// 加载样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
